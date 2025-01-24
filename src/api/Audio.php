6e59e4f94da93dae6d3968cfce67bb6937e93497
yunwuxin <448901948@qq.com>
2025-01-24 15:55:33 +0800

    更新

--- src/api/Audio.php
+++ src/api/Audio.php
@@ -2,7 +2,6 @@
 
 namespace think\ai\api;
 
-use SplFileInfo;
 use think\ai\Api;
 
 class Audio extends Api
@@ -16,26 +15,8 @@ class Audio extends Api
 
     public function transcriptions($params)
     {
-        $multipart = [];
-
-        foreach ($params as $key => $value) {
-            if ($value instanceof SplFileInfo) {
-                $filename    = method_exists($value, 'getOriginalName') ? $value->getOriginalName() : $value->getBasename();
-                $multipart[] = [
-                    'name'     => $key,
-                    'contents' => $value->openFile('r'),
-                    'filename' => $filename,
-                ];
-            } else {
-                $multipart[] = [
-                    'name'     => $key,
-                    'contents' => $value,
-                ];
-            }
-        }
-
         return $this->request('POST', 'audio/transcriptions', [
-            'multipart' => $multipart,
+            'json' => $params,
         ]);
     }
 
