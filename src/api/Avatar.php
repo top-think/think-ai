7e1a82d07c0b19fc66512b75867c215f3a8d466e
yunwuxin <448901948@qq.com>
2025-12-10 00:51:06 +0800

    feat(api): 支持avatar视频生成时的同步等待功能

--- src/api/Avatar.php
+++ src/api/Avatar.php
@@ -3,14 +3,43 @@
 namespace think\ai\api;
 
 use think\ai\Api;
+use think\ai\Exception;
+use think\helper\Arr;
 
 class Avatar extends Api
 {
-    public function video($params)
+    public function video($params, $wait = false)
     {
-        return $this->request('POST', 'avatar/video', [
+        $result = $this->request('POST', 'avatar/video', [
             'json' => $params,
         ]);
+
+        if (!$wait) {
+            return $result;
+        }
+
+        $id = $result['id'];
+
+        if (!is_int($wait)) {
+            $wait = 300;
+        }
+
+        $start = time();
+        while (true) {
+            try {
+                $result = $this->query(['id' => $id]);
+                if (Arr::get($result, 'status') != 'processing') {
+                    return $result;
+                }
+            } catch (\Throwable) {
+
+            }
+            if (time() - $start > $wait) {
+                throw new Exception('Timeout');
+            }
+            sleep(5);
+        }
+
     }
 
     public function query($params)
